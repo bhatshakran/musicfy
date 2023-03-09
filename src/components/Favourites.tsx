67631a6746fcb3f0d7d48a4511a3128f8d960868
bhatshakran <bhatshakran@gmail.com>
2023-03-09 21:07:21 +0530

    added gif file

--- src/components/Favourites.tsx
+++ src/components/Favourites.tsx
@@ -8,7 +8,7 @@ const Favourites = () => {
     <div className='pt-3 overflow-scroll' style={{ height: '600px' }}>
       <h2 className='opensansbold ms-5'>Your Favourites</h2>
       {!isEmpty(favouritesState) ? (
-        <Grid tracks={favouritesState}></Grid>
+        <Grid tracks={favouritesState} more={true}></Grid>
       ) : (
         <div className='ms-5'>
           No favourites yet. Search for songs and add to favs .
--- src/helpers/PrivateRoute.tsx
+++ src/helpers/PrivateRoute.tsx
@@ -1,12 +1,33 @@
 import { useKeycloak } from '@react-keycloak/web';
+import { useEffect, useState } from 'react';
 import ErrorPage from '../components/404';
+import { Loading } from '../components/Loading';
 
 const PrivateRoute = ({ children }: any) => {
   const { keycloak } = useKeycloak();
 
   const isLoggedIn = keycloak.authenticated;
+  const [loading, setLoading] = useState(true);
 
-  return isLoggedIn ? children : <ErrorPage />;
+  useEffect(() => {
+    if (isLoggedIn) {
+      setLoading(false);
+    }
+  }, [isLoggedIn]);
+
+  if (loading) {
+    return (
+      <div className='w-100 d-flex vh-100 justify-content-center align-items-center'>
+        <div className='w-50'>
+          <Loading />;
+        </div>
+      </div>
+    );
+  } else if (isLoggedIn) {
+    return children;
+  } else {
+    return <ErrorPage />;
+  }
 };
 
 export default PrivateRoute;
